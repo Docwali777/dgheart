b9529bebf2293bdc7db28af8b2486de9ab2662da
docwali777 <drgauvin@mgmcare.org>
2017-10-26 12:46:30 -0400

    build with pics

--- server/server.js
+++ server/server.js
@@ -9,9 +9,16 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
 
-app.get('/*', (req, res)=>{
-  res.sendFile(path.resolve(__dirname, '../public', 'index.html'))
-})
+app.get('*.js', function (req, res, next) {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  next();
+});
+
+//
+// app.get('/*', (req, res)=>{
+//   res.sendFile(path.resolve(__dirname, '../public', 'index.html'))
+// })
 
 app.listen(PORT, ()=>{
   console.log(`server on PORT: ${PORT}`);
